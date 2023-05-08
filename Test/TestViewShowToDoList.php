e10f473b7a9b758e6689e579dcd9304924c2fcb8
rofiadhimPS <muhammadrofiadhim@gmail.com>
2023-05-09 00:19:22 +0800

    implementasi View 1

---
+++ Test/TestViewShowToDoList.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "../View/ViewShowToDoList.php";
+require_once "../BusinessLogic/AddToDoList.php";
+
+addToDoList("Muh.");
+addToDoList("Rofiadhim");
+addToDoList("Rajab");
+
+viewShowToDoList();
+
--- View/ViewShowToDoList.php
+++ View/ViewShowToDoList.php
@@ -1,6 +1,33 @@
 <?php
 
+require_once "../Model/ToDoList.php";
+require_once "../BusinessLogic/ShowToDoList.php";
+require_once "../View/ViewAddToDoList.php";
+require_once "../View/ViewRemoveToDoList.php";
+require_once "../Helper/Input.php";
+
 function viewShowToDoList()
 {
-    
+    while (true) {
+        showToDoList();
+
+        echo "MENU" . PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = input("Pilih"); 
+
+        if ($pilihan == "1"){
+            viewAddToDoList();
+        } else if ($pilihan == "2") {
+            viewRemoveToDoList();
+        } else if ($pilihan == "x") {
+            break;
+        } else {
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
 }
